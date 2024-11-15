ea1a1481c1dd9e8f035f0720116d2dcc66283eff
yagosana <yagosana@ucm.es>
2024-11-15 16:36:15 +0100

    aviso legal

--- public_html/avisoLegal.php
+++ public_html/avisoLegal.php
@@ -5,7 +5,29 @@ $titulo = 'Aviso Legal OINK!';
 $contenido = <<<EOS
         <article>
             <section>
-                <h1>Aviso Legal</h1>       
+                <div id='avisoLegal'>
+                    <h1>Aviso Legal</h1>
+                    <p>
+                        Oink!® es una marca registrada en la Oficina Española de Patentes y Marcas (OEPM), protegida por las leyes de propiedad intelectual aplicables.
+                    </p>
+                    <p>
+                        <strong>Tipo de Marca: </strong> La marca Oink! es una marca individual, verbal y denominativa registrada bajo la clasificación de Niza 25.
+                    </p>
+                        <strong>Derechos de Propiedad Intelectual: </strong> La marca Oink! está protegida por las leyes de propiedad intelectual. Cualquier uso no autorizado de la marca Oink! 
+                        sin permiso expreso está estrictamente prohibido y puede resultar en acciones legales.
+                    </p>
+                    <p>
+                        <strong>Uso No Autorizado: </strong> Queda estrictamente prohibido el uso de la marca Oink! para identificar productos o servicios no autorizados. Cualquier uso no 
+                        autorizado, incluyendo la reproducción, imitación, modificación o explotación comercial de la marca Oink! será perseguido legalmente.
+                    </p>
+                    <p>
+                        <strong>Responsabilidad Legal: </strong> No nos hacemos responsables de los daños o perjuicios que puedan derivarse del uso indebido de la marca Oink! por parte de 
+                        terceros no autorizados.
+                    </p>
+                    <p>
+                        <strong>Actualización del Aviso: </strong> Este aviso legal puede ser actualizado periódicamente.
+                    </p>
+                </div>   
             </section>
         </article>
     EOS;
